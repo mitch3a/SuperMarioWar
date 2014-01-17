d44f9b7347871e154524d87134c7982ebcdd1d0b
metapod <rgrawin@gmail.com>
2014-01-17 15:37:24 -0500

    Sort of a lazy commit, but I don't want to forget about making a font class

---
+++ src/smw/gfx/Font.java
@@ -0,0 +1,17 @@
+package smw.gfx;
+
+public class Font {
+  // TODO - Need a font class that can use a font "sprite sheet" to output letters
+  // I'm guessing we will need this for the score, menus, and whatever else has writing...
+  
+  public Font() {
+    
+  }
+  
+  public void init() {
+    
+  }
+  
+  
+  
+}
