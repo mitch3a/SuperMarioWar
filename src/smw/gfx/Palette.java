86ae757d12203cb581542386c5cc85b1b2b28346
Mitchell Kendall <mitchKendall@gmail.com>
2014-01-19 18:25:27 -0500

    Can now change color (teams)... and some collision

--- src/smw/gfx/Palette.java
+++ src/smw/gfx/Palette.java
@@ -2,24 +2,42 @@ package smw.gfx;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.HashMap;
 
 import javax.imageio.ImageIO;
 
-/* Stole this from the C++ stuff... didn't quite finish
+//This class is a singleton because you only need one.
 public class Palette {
+  private static Palette instance;
 
-	public Palette(){
-		
-	}
+  static {
+      instance = new Palette();
+  }
+
+  private Palette() { 
+      loadPalette();
+  }    
+
+  public static Palette getInstance() {
+      return instance;
+  }
 	
-  //[numplayers][colorscheme][colorcomponents][numcolors]
-	byte[][][][] colorschemes;
-	//[colorcomponent][code]
-	byte[][]   colorCodes;
+  public enum ColorScheme{
+    RED(0), GREEN(1), YELLOW(2), BLUE(3);
+    
+    public final int index;
+    private ColorScheme(int index) {
+      this.index = index;
+    }
+  }
+  
+	HashMap<Integer, int[]> colorMap = new HashMap<Integer, int[]>();
+	
 	final static int NUM_SCHEMES = 9;
 	final static int redIndex = 0;
 	final static int greenIndex = 1;
-	final static int blueIndex = 2;
+	final static int yellowIndex = 2;
+	final static int blueIndex = 3;
 	
 	byte getRed(int color){
 		return (byte) ((color >> 16) & 0x000000FF);
@@ -34,105 +52,38 @@ public class Palette {
 	}
 	
 	public void loadPalette(){
+	  int[] alphaInt = {0, 0, 0, 0};
+	  colorMap.put(0xffff00ff, alphaInt);
 		try{
 			BufferedImage imageBuffer = ImageIO.read(this.getClass().getClassLoader().getResource("sprites/palette.bmp"));
 			
 			final int width = imageBuffer.getWidth();
-		  final int height = imageBuffer.getHeight();
-		  int[] pixels = new int[width * height];
-		  imageBuffer.getRGB(0, 0, width, height, pixels, 0, width);
 
-		  int numColors = pixels.length;
-		  
-		  colorCodes = new byte[3][width];
-		  colorschemes = new byte[4][NUM_SCHEMES][3][width];
-			
-		  //The first row contains the color codes
-			for(int k = 0; k < width; k++){
-				int color = pixels[k];
-				colorCodes[redIndex][k]   = getRed(color);
-				colorCodes[greenIndex][k] = getGreen(color);
-				colorCodes[blueIndex][k]  = getBlue(color);
-			}
-			
-			int pixelCounter = width; //Skip the first row because it's the colorCode
-			//The rest is the color scheme
-			for(int i = 0; i < 4; ++i)
-			{
-				for(int j = 0; j < NUM_SCHEMES; ++j)
-				{
-					for(int m = 0; m < width; ++m)
-					{
-						int color = pixels[pixelCounter++];
-						colorschemes[i][j][redIndex][m]   = getRed(color);
-						colorschemes[i][j][greenIndex][m] = getGreen(color);
-						colorschemes[i][j][blueIndex][m]  = getBlue(color);
-					}
-				}
-			}	  
+		  //These values are strictly based on what I saw in the palette image -mk
+		  for(int i = 0 ; i < width ; ++i){
+		    int baseColor = imageBuffer.getRGB(i, 0);   //Key
+		    int[] values = {imageBuffer.getRGB(i,  1),  //Red
+		                    imageBuffer.getRGB(i, 10),  //Green
+		                    imageBuffer.getRGB(i, 19),  //Yellow
+		                    imageBuffer.getRGB(i, 28)}; //Blue
+		    colorMap.put(baseColor, values);
+		  }
 		} catch (IOException e) {
       e.printStackTrace();  // TODO UH OH...error handling...
     }
 	}
 	
-
-	void createSkin(BufferedImage[][] image, Uint8 r, Uint8 g, Uint8 b, short colorScheme, bool expand)
-	{
-		for(int j = 0; j < image.length; j++)
-		{
-			for(int i = 0; i < image[0].length ; i++)
-			{
-				BufferedImage imageBuffer = image[j][i];
-		
-				final int width = imageBuffer.getWidth();
-			  final int height = imageBuffer.getHeight();
-			  int[] pixels = new int[width * height];
-			  imageBuffer.getRGB(0, 0, width, height, pixels, 0, width);
-			  
-			  for(int currentPixel = 0 ; currentPixel < pixels.length ; ++currentPixel){
-			  	int color = pixels[currentPixel];
-			  	byte red   = getRed(color);
-					byte green = getGreen(color);
-					byte blue  = getBlue(color);
-					
-					bool fFoundColor = false;
-					for(int m = 0; m < numcolors; m++){
-						//TODO mk  there better be a reason why color codes is not just an int...
-						if(red == colorCodes[redIndex][m] && green == colorCodes[greenIndex][m] && blue == colorCodes[blueIndex][m])
-						{
-							for(int k = 0; k < NUM_SCHEMES; k++)
-							{
-								temppixels[tempcounter + k * 96 + reverseoffset + iRedOffset] = colorschemes[colorScheme][k][0][m];
-								temppixels[tempcounter + k * 96 + reverseoffset + iGreenOffset] = colorschemes[colorScheme][k][1][m];
-								temppixels[tempcounter + k * 96 + reverseoffset + iBlueOffset] = colorschemes[colorScheme][k][2][m];
-							}
-							
-							fFoundColor = true;
-							break;
-						}
-					}
-			  }
-
-				if(!fFoundColor)
-				{
-					for(int k = 0; k < loops; k++)
-					{
-						temppixels[tempcounter + k * 96 + reverseoffset + iRedOffset] = iColorByte1;
-						temppixels[tempcounter + k * 96 + reverseoffset + iGreenOffset] = iColorByte2;
-						temppixels[tempcounter + k * 96 + reverseoffset + iBlueOffset] = iColorByte3;
-					}
-				}
-	
-				skincounter += 3;
-				tempcounter += 3;
-			}
-	
-			skincounter += 480 + skin->pitch - (skin->w * 3);
-			tempcounter += 96 * (loops - 1) + temp->pitch - (temp->w * 3);
-		}
-
-	
-		return final;
+	/** This also removes megenta (into alpha zero) **/
+	public void colorSprite(ColorScheme color, BufferedImage image){
+	  int colorIndex = color.index;
+	  
+	  for(int w = 0 ; w < image.getWidth() ; ++w){
+	    for(int h = 0 ; h < image.getHeight(); ++h){
+	      int currentColor = image.getRGB(w, h);
+	      if(colorMap.containsKey(currentColor)){
+	        image.setRGB(w, h, colorMap.get(currentColor)[colorIndex]);
+	      }
+	    }
+	  }
 	}
 }
-*/
