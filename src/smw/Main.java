2ce0e9e76aee55cb57cd5fec8a9466b47314bd78
mitch3a <mitch3apps@gmail.com>
2014-01-18 22:17:22 -0500

    started color mapping but not even close to finished

--- src/smw/Main.java
+++ src/smw/Main.java
@@ -1,11 +1,8 @@
 package smw;
 
-import smw.gfx.Palette;
-
 public class Main {
 //here is a comment
 	public static void main(String[] args) {
-		new Palette().loadPalette();
 		int numPlayers = 2;
 		new Game(numPlayers).start();
 	}
--- src/smw/gfx/Palette.java
+++ src/smw/gfx/Palette.java
@@ -5,15 +5,33 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
+/* Stole this from the C++ stuff... didn't quite finish
 public class Palette {
 
 	public Palette(){
 		
 	}
 	
+  //[numplayers][colorscheme][colorcomponents][numcolors]
+	byte[][][][] colorschemes;
+	//[colorcomponent][code]
 	byte[][]   colorCodes;
-	byte[][][] colorschemes;
-	final int NUM_SCHEMES = 3;
+	final static int NUM_SCHEMES = 9;
+	final static int redIndex = 0;
+	final static int greenIndex = 1;
+	final static int blueIndex = 2;
+	
+	byte getRed(int color){
+		return (byte) ((color >> 16) & 0x000000FF);
+	}
+	
+	byte getGreen(int color){
+		return (byte) ((color >> 8) & 0x000000FF);
+	}
+	
+	byte getBlue(int color){
+		return (byte) ((color) & 0x000000FF);
+	}
 	
 	public void loadPalette(){
 		try{
@@ -23,67 +41,98 @@ public class Palette {
 		  final int height = imageBuffer.getHeight();
 		  int[] pixels = new int[width * height];
 		  imageBuffer.getRGB(0, 0, width, height, pixels, 0, width);
-		  
+
 		  int numColors = pixels.length;
 		  
-		  colorCodes = new byte[3][numColors];
-		  colorschemes = new byte[4][NUM_SCHEMES][3];
-		  
-		  for(int k = 0; k < 3 ; ++k){
-		  	for(int i = 0; i < 4; ++i)
-					for(int j = 0; j < NUM_SCHEMES; ++j)
-						colorschemes[i][j][k] = new Uint8[numcolors];;
-		  }
-		  
+		  colorCodes = new byte[3][width];
+		  colorschemes = new byte[4][NUM_SCHEMES][3][width];
+			
+		  //The first row contains the color codes
+			for(int k = 0; k < width; k++){
+				int color = pixels[k];
+				colorCodes[redIndex][k]   = getRed(color);
+				colorCodes[greenIndex][k] = getGreen(color);
+				colorCodes[blueIndex][k]  = getBlue(color);
+			}
+			
+			int pixelCounter = width; //Skip the first row because it's the colorCode
+			//The rest is the color scheme
+			for(int i = 0; i < 4; ++i)
+			{
+				for(int j = 0; j < NUM_SCHEMES; ++j)
+				{
+					for(int m = 0; m < width; ++m)
+					{
+						int color = pixels[pixelCounter++];
+						colorschemes[i][j][redIndex][m]   = getRed(color);
+						colorschemes[i][j][greenIndex][m] = getGreen(color);
+						colorschemes[i][j][blueIndex][m]  = getBlue(color);
+					}
+				}
+			}	  
 		} catch (IOException e) {
       e.printStackTrace();  // TODO UH OH...error handling...
     }
 	}
-		/*
-		for(int k = 0; k < 3; k++)
+	
+
+	void createSkin(BufferedImage[][] image, Uint8 r, Uint8 g, Uint8 b, short colorScheme, bool expand)
+	{
+		for(int j = 0; j < image.length; j++)
 		{
-			colorcodes[k] = new Uint8[numcolors];
-
-			for(int i = 0; i < 4; i++)
-				for(int j = 0; j < NUM_SCHEMES; j++)
-					colorschemes[i][j][k] = new Uint8[numcolors];
-		}
-
-		if(SDL_MUSTLOCK(palette))
-			SDL_LockSurface(palette);
-
-		int counter = 0;
-
-		Uint8 * pixels = (Uint8*)palette->pixels;
-
-		short iRedIndex = palette->format->Rshift >> 3;
-		short iGreenIndex = palette->format->Gshift >> 3;
-		short iBlueIndex = palette->format->Bshift >> 3;
-
-		for(int k = 0; k < numcolors; k++)
-		{
-			colorcodes[iRedIndex][k] = pixels[counter++];
-			colorcodes[iGreenIndex][k] = pixels[counter++];
-			colorcodes[iBlueIndex][k] = pixels[counter++];
-		}
-
-		counter += palette->pitch - palette->w * 3;
-
-		for(int i = 0; i < 4; i++)
-		{
-			for(int j = 0; j < NUM_SCHEMES; j++)
+			for(int i = 0; i < image[0].length ; i++)
 			{
-				for(int m = 0; m < numcolors; m++)
-				{
-					colorschemes[i][j][iRedIndex][m] = pixels[counter++];
-					colorschemes[i][j][iGreenIndex][m] = pixels[counter++];
-					colorschemes[i][j][iBlueIndex][m] = pixels[counter++];
-				}
-
-				counter += palette->pitch - palette->w * 3;
-			}
-		}
+				BufferedImage imageBuffer = image[j][i];
 		
-		return true;
-	}*/
+				final int width = imageBuffer.getWidth();
+			  final int height = imageBuffer.getHeight();
+			  int[] pixels = new int[width * height];
+			  imageBuffer.getRGB(0, 0, width, height, pixels, 0, width);
+			  
+			  for(int currentPixel = 0 ; currentPixel < pixels.length ; ++currentPixel){
+			  	int color = pixels[currentPixel];
+			  	byte red   = getRed(color);
+					byte green = getGreen(color);
+					byte blue  = getBlue(color);
+					
+					bool fFoundColor = false;
+					for(int m = 0; m < numcolors; m++){
+						//TODO mk  there better be a reason why color codes is not just an int...
+						if(red == colorCodes[redIndex][m] && green == colorCodes[greenIndex][m] && blue == colorCodes[blueIndex][m])
+						{
+							for(int k = 0; k < NUM_SCHEMES; k++)
+							{
+								temppixels[tempcounter + k * 96 + reverseoffset + iRedOffset] = colorschemes[colorScheme][k][0][m];
+								temppixels[tempcounter + k * 96 + reverseoffset + iGreenOffset] = colorschemes[colorScheme][k][1][m];
+								temppixels[tempcounter + k * 96 + reverseoffset + iBlueOffset] = colorschemes[colorScheme][k][2][m];
+							}
+							
+							fFoundColor = true;
+							break;
+						}
+					}
+			  }
+
+				if(!fFoundColor)
+				{
+					for(int k = 0; k < loops; k++)
+					{
+						temppixels[tempcounter + k * 96 + reverseoffset + iRedOffset] = iColorByte1;
+						temppixels[tempcounter + k * 96 + reverseoffset + iGreenOffset] = iColorByte2;
+						temppixels[tempcounter + k * 96 + reverseoffset + iBlueOffset] = iColorByte3;
+					}
+				}
+	
+				skincounter += 3;
+				tempcounter += 3;
+			}
+	
+			skincounter += 480 + skin->pitch - (skin->w * 3);
+			tempcounter += 96 * (loops - 1) + temp->pitch - (temp->w * 3);
+		}
+
+	
+		return final;
+	}
 }
+*/
