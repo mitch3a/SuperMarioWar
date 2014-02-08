7f832cabdd3c8b6ea4359f2e532da85468ce1dbf
mitch3a <mitch3apps@gmail.com>
2014-02-08 14:01:24 -0500

    Make AnimatedBlock inherit from SolidBlock. Implement the differences later.

---
+++ src/smw/world/blocks/AnimatedBlock.java
@@ -0,0 +1,107 @@
+package smw.world.blocks;
+
+import smw.Updatable;
+import smw.world.Tile;
+
+public abstract class AnimatedBlock extends SolidBlock implements Updatable{
+  /** The frame rate of the animation. */
+  protected int frameRate;
+  /** The time per frame. */
+  protected int updateRate_ms;
+  /** Time it takes for a complete animation (all frames). */
+  protected int animationTime_ms;
+  /** The total time that has elapsed (for all frames). */
+  private int elapsedTime_ms;
+  /** The time that has elapsed for the current frame. */
+  private int frameTime_ms;
+  /** Indicates if the animation is running. */
+  private boolean running = true;
+  
+  public AnimatedBlock(int x, int y, String tileSheet) {
+    super(x, y, tileSheet);
+    
+    frameRate = 4;
+    updateRate_ms = 1000/frameRate;
+    
+    animationTime_ms = updateRate_ms*this.tileSheet.getWidth()/Tile.SIZE;
+  }
+
+  @Override
+  public void update(float timeDif_ms) {
+    if (!running) {
+      return;
+    }
+    
+    elapsedTime_ms += timeDif_ms;
+    frameTime_ms += timeDif_ms;
+    
+    if (elapsedTime_ms >= animationTime_ms) {
+      elapsedTime_ms = 0;
+      frameTime_ms = 0;
+      subImageX = 0;
+    }
+    
+    if (frameTime_ms >= updateRate_ms) {
+      frameTime_ms = 0;
+      subImageX += Tile.SIZE;
+    }
+  }
+  
+  public static class BreakableBlock extends AnimatedBlock{
+    public BreakableBlock(int x, int y){
+      super(x, y, "breakableblock.png");
+    }
+  }
+  
+  public static class QuestionBlock extends AnimatedBlock{
+    public QuestionBlock(int x, int y){
+      super(x, y, "powerupblock.png");
+    }
+  }
+  
+  public static class FlipBlock extends AnimatedBlock{
+    public FlipBlock(int x, int y){
+      super(x, y, "flipblock.png");
+    }
+  }
+  
+  public static class BlueNoteBlock extends AnimatedBlock{
+    public BlueNoteBlock(int x, int y){
+      super(x, y, "noteblock.png");
+    }
+  }
+  
+  public static class WhiteNoteBlock extends AnimatedBlock{
+    public WhiteNoteBlock(int x, int y){
+      super(x, y, "noteblock.png");
+      this.subImageY += Tile.SIZE;
+    }
+  }
+  
+  public static class RedNoteBlock extends AnimatedBlock{
+    public RedNoteBlock(int x, int y){
+      super(x, y, "noteblock.png");
+      this.subImageY += 2*Tile.SIZE;
+    }
+  }
+  
+  public static class BlueThrowBlock extends AnimatedBlock{
+    public BlueThrowBlock(int x, int y){
+      super(x, y, "throwblock.png");
+    }
+  }
+  
+  public static class WhiteThrowBlock extends AnimatedBlock{
+    public WhiteThrowBlock(int x, int y){
+      super(x, y, "throwblock.png");
+      this.subImageY += Tile.SIZE;
+    }
+  }
+  
+  public static class RedThrowBlock extends AnimatedBlock{
+    public RedThrowBlock(int x, int y){
+      super(x, y, "throwblock.png");
+      this.subImageY += 2*Tile.SIZE;
+    }
+  }
+}
