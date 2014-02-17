122739f70b1cfd6bf771692f180fb9fe70d60840
metapod <rgrawin@gmail.com>
2014-02-16 20:37:58 -0500

    Little closer to looking like the real menu, lil' sloppy though!

--- src/smw/menu/TitleMenu.java
+++ src/smw/menu/TitleMenu.java
@@ -13,18 +13,21 @@ import smw.gfx.Font;
 import smw.gfx.Palette;
 import smw.ui.screen.GameFrame;
 
-public class TitleMenu extends Menu {
-
+public class TitleMenu extends Menu { 
   private BufferedImage backgroundImg;
-  private BufferedImage selectFieldImg;
+  private BufferedImage selectFieldImg; // TODO - probably don't need this big img hanging around
   
-  BufferedImage leftPipe;
-  BufferedImage middlePipe;
-  BufferedImage rightPipe;
+  private BufferedImage leftPipe;
+  private BufferedImage middlePipe;
+  private BufferedImage rightPipe;
+  
+  private BufferedImage leftField;
+  private BufferedImage middleField;
+  private BufferedImage rightField;
   
   private int selection;
   
-  
+  private boolean setupSound = true; 
     
   public TitleMenu() {
     try {
@@ -43,7 +46,7 @@ public class TitleMenu extends Menu {
       Graphics2D g = backgroundImg.createGraphics();
       g.drawImage(titleImg, (backgroundImg.getWidth() - titleImg.getWidth()) / 2, 30, null);
       
-      // Read the sprite sheet for the menu fields.
+      // Read the sprite sheet for the pipe menu fields.
       tempImg = ImageIO.read(this.getClass().getClassLoader().getResource("menu/menu_selectfield.png"));
       selectFieldImg = new BufferedImage(tempImg.getWidth(), tempImg.getHeight(), BufferedImage.TYPE_4BYTE_ABGR);
       selectFieldImg.getGraphics().drawImage(tempImg, 0, 0, null);
@@ -52,22 +55,25 @@ public class TitleMenu extends Menu {
       leftPipe = selectFieldImg.getSubimage(0, 0, 32, 32);
       middlePipe = selectFieldImg.getSubimage(32, 0, 32, 32);
       rightPipe = selectFieldImg.getSubimage(32 * 15, 0, 32, 32);
+      
+      drawPipeField(g, "Start", 7, 120, 210, true);
+      drawPipeField(g, "Go!", 0, 440, 210, false);
+      drawPipeField(g, "Options", 10, 120, 323, true);
+      drawPipeField(g, "Controls", 10, 120, 363, true);
+      drawPipeField(g, "Exit", 10, 120, 402, true);
+      
+      // Read the sprite sheet for the player select menu field.
+      tempImg = ImageIO.read(this.getClass().getClassLoader().getResource("menu/menu_player_select.png"));
+      BufferedImage playerSelect = new BufferedImage(tempImg.getWidth(), tempImg.getHeight(), BufferedImage.TYPE_4BYTE_ABGR);
+      playerSelect.getGraphics().drawImage(tempImg, 0, 0, null);
+      p.implementTransparent(playerSelect);
+      
+      leftField = playerSelect.getSubimage(0, 0, 16, 64);
+      middleField = playerSelect.getSubimage(32, 0, 16, 64);
+      rightField = playerSelect.getSubimage(32 * 15 + 16, 0, 16, 64);
+      
+      drawPlayerSelectField(g);
 
-      /*
-      drawPipeField(g, "Start", 8, 137, 240);
-      drawPipeField(g, "Go!", 1, 457, 240);
-      
-      drawPipeField(g, "Options", 10, 137, 353);
-      drawPipeField(g, "Controls", 10, 137, 393);
-      drawPipeField(g, "Exit", 10, 137, 432);
-      */
-      drawPipeField(g, "Start", 8, 137 - 7, 240 - 30);
-      drawPipeField(g, "Go!", 1, 457 - 7, 240 - 30);
-      
-      drawPipeField(g, "Options", 10, 137 - 7, 353 - 30);
-      drawPipeField(g, "Controls", 10, 137 - 7, 393 - 30);
-      drawPipeField(g, "Exit", 10, 137 - 7, 432 - 30);
-      
     } catch (IOException e) {
       e.printStackTrace();
     }
@@ -75,20 +81,62 @@ public class TitleMenu extends Menu {
   
   
   // TODO - maybe add an enum value for pipe type to draw!
-  public void drawPipeField(Graphics2D g, String text, int length, int x, int y) {
+  public void drawPipeField(Graphics2D g, String text, int length, int x, int y, boolean extra) {
     int textX = x + 24;
     int textY = y + 4;
     g.drawImage(leftPipe, x, y, null);
     x+= leftPipe.getWidth();
     for (int i = 0; i < length; i++) {
-      g.drawImage(middlePipe, x + (i * middlePipe.getWidth()), y, null);
+      g.drawImage(middlePipe, x, y, null);
+      x += middlePipe.getWidth();
     }
-    g.drawImage(rightPipe, x + (length * middlePipe.getWidth()), y, null);
+    if (extra) { // TODO - this is awful, should really just be able to give a length in pixels for a field then just draw it!
+      x-=16;
+      g.drawImage(middlePipe, x, y, null);
+      x += middlePipe.getWidth();
+    } else {
+      g.drawImage(middlePipe, x, y, null);
+      x +=16;
+    }
+    
+    g.drawImage(rightPipe, x, y, null);
     
     if (text != null) {
-      Font font = Font.getInstance();
-      font.drawLargeText(g, text, textX, textY, null);
+      Font.getInstance().drawLargeText(g, text, textX, textY, null);
     }
+  }
+  
+  // TODO
+  public void drawPlayerSelectField(Graphics2D g) {
+    int x = 120;
+    int y = 250;
+    Font font = Font.getInstance();
+    
+    g.drawImage(leftField, x, y, null);
+    x+= leftField.getWidth();
+    for (int i = 0; i < 7; i++) {
+      g.drawImage(middleField, x, y, null);
+      x+=middleField.getWidth();
+    }
+    g.drawImage(rightField, x, y, null);
+    x+= rightField.getWidth();
+    font.drawLargeText(g, "Players", 128+16, y+18, null);
+    
+    x-=3; // Nudge player select up against first box.
+    g.drawImage(leftField, x, y, null);
+    x+= leftField.getWidth();
+    for (int i = 0; i < 14; i++) {
+      g.drawImage(middleField, x, y, null);
+      x+=middleField.getWidth();
+    }
+    x-=13;
+    g.drawImage(middleField, x, y, null);
+    x+=middleField.getWidth();
+    
+    g.drawImage(rightField, x, y, null);
+    x+= rightField.getWidth();
+    
+    // TODO - get and store player, bot, none icons, also small menu text for font..., selection animations
   }
   
   @Override
@@ -99,7 +147,9 @@ public class TitleMenu extends Menu {
 
   @Override
   public void update() {
-    //Game.soundPlayer.playMenuMusic();
+    if (setupSound) {
+      Game.soundPlayer.playMenuMusic();
+    }
     // TODO - update based on user selection
   }
   
