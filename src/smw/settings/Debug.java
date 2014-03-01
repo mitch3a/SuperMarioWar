0d761af3d8f28753357354f4ba567e2a8d68ecb8
metapod <rgrawin@gmail.com>
2014-03-01 13:01:09 -0500

    Implemented player idle death condition - OFF for now in Debug class

--- src/smw/settings/Debug.java
+++ src/smw/settings/Debug.java
@@ -12,6 +12,7 @@ public class Debug {
   public static final boolean MUTE = false;
   public static final boolean MUTE_SFX = false;
   public static final boolean MUTE_MUSIC = true;
+  public static boolean PLAYER_DEATH_IDLE = false; 
   
   /*
    * Prints the map name, version, and tilesets
