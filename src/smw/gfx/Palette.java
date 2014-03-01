9857a21e4ffb383d1b30ce9d617dc25e4feb03e3
metapod <rgrawin@gmail.com>
2014-03-01 16:26:23 -0500

    Fixed issue where certain sprites retained magenta pixels after magenta to transparent conversion.

--- src/smw/gfx/Palette.java
+++ src/smw/gfx/Palette.java
@@ -14,24 +14,22 @@ import javax.imageio.ImageIO;
  * implementing the team color and replacing "magenta" 
  * with transparency.
  *****************************************************/
-
-//This class is a singleton because you only need one.
 public class Palette {
   private static Palette instance;
 
   static {
-      instance = new Palette();
+    instance = new Palette();
   }
 
   private Palette() { 
-      loadPalette();
+    loadPalette();
   }    
 
   public static Palette getInstance() {
-      return instance;
+    return instance;
   }
 	
-  public enum ColorScheme{
+  public enum ColorScheme {
     RED(0), GREEN(1), YELLOW(2), BLUE(3);
     
     public final int index;
@@ -48,28 +46,27 @@ public class Palette {
 	final static int yellowIndex = 2;
 	final static int blueIndex = 3;
 	
-	byte getRed(int color){
+	byte getRed(int color) {
 		return (byte) ((color >> 16) & 0x000000FF);
 	}
 	
-	byte getGreen(int color){
+	byte getGreen(int color) {
 		return (byte) ((color >> 8) & 0x000000FF);
 	}
 	
-	byte getBlue(int color){
+	byte getBlue(int color) {
 		return (byte) ((color) & 0x000000FF);
 	}
 	
-	public void loadPalette(){
+	public void loadPalette() {
 	  int[] alphaInt = {0x00000000, Color.TRANSLUCENT, 0x00000000, 0x00000000};
 	  colorMap.put(0xffff00ff, alphaInt);
 		try{
 			BufferedImage imageBuffer = ImageIO.read(this.getClass().getClassLoader().getResource("palette.png"));
-			
 			final int width = imageBuffer.getWidth();
 
 		  //These values are strictly based on what I saw in the palette image -mk
-		  for(int i = 0 ; i < width ; ++i){
+		  for(int i = 0 ; i < width ; ++i) {
 		    int baseColor = imageBuffer.getRGB(i, 0);   //Key
 		    int[] values = {imageBuffer.getRGB(i,  1),  //Red
 		                    imageBuffer.getRGB(i, 10),  //Green
@@ -78,15 +75,15 @@ public class Palette {
 		    colorMap.put(baseColor, values);
 		  }
 		} catch (IOException e) {
-      e.printStackTrace();  // TODO UH OH...error handling...
+      e.printStackTrace();
     }
 	}
 
-	public void applyAlpha(BufferedImage image, int alpha){
-	  for(int w = 0 ; w < image.getWidth() ; ++w){
-      for(int h = 0 ; h < image.getHeight(); ++h){
+	public void applyAlpha(BufferedImage image, int alpha) {
+	  for (int w = 0 ; w < image.getWidth() ; ++w) {
+      for (int h = 0 ; h < image.getHeight(); ++h) {
         int currentColor = image.getRGB(w, h);
-        if(currentColor != Color.TRANSLUCENT){
+        if (currentColor != Color.TRANSLUCENT) {
           currentColor = currentColor << 8;
           currentColor = currentColor >> 8;
           currentColor += (alpha << 24);
@@ -96,58 +93,66 @@ public class Palette {
     }
 	}
 
-	/** This also removes megenta (into alpha zero) **/
-	public void colorSprite(ColorScheme color, BufferedImage image){
-	  int colorIndex = color.index;
-	  
-	  for(int w = 0 ; w < image.getWidth() ; ++w){
-	    for(int h = 0 ; h < image.getHeight(); ++h){
+	/**
+	 * Colors the provided sprite image based on the provided color scheme.
+	 * This also converts megenta to transparent (alpha channel).
+	 * @param color The color scheme.
+	 * @param image The sprite image.
+	 */
+	public void colorSprite(ColorScheme color, BufferedImage image) {	  
+	  for (int w = 0 ; w < image.getWidth() ; ++w) {
+	    for (int h = 0 ; h < image.getHeight(); ++h) {
 	      int currentColor = image.getRGB(w, h);
-	      if(colorMap.containsKey(currentColor)){
-	        image.setRGB(w, h, colorMap.get(currentColor)[colorIndex]);
+	      if (colorMap.containsKey(currentColor) ){
+	        image.setRGB(w, h, colorMap.get(currentColor)[color.index]);
 	      }
+	      
+	      if (image.getRGB(w, h) == 0xffff00ff) {
+          image.setRGB(w, h, Color.TRANSLUCENT);
+        }
 	    }
 	  }
 	}
-
+	
 	 public void implementTransparent(BufferedImage image, int colorToRemove) {
-	    for(int w = 0 ; w < image.getWidth() ; ++w){
-	      for(int h = 0 ; h < image.getHeight(); ++h){
-	        if(image.getRGB(w, h) == colorToRemove){
-	          image.setRGB(w, h, Color.TRANSLUCENT);
-	        }
-	      }
-	    } 
-	  }
+    for (int w = 0 ; w < image.getWidth() ; ++w) {
+      for (int h = 0 ; h < image.getHeight(); ++h) {
+        if (image.getRGB(w, h) == colorToRemove) {
+          image.setRGB(w, h, Color.TRANSLUCENT);
+        }
+      } 
+    } 
+	}
 	 
 	public void implementTransparent(BufferedImage image) {
+	  // 0xFFFF00FF
 	  implementTransparent(image, 0xffff00ff);
   }
 	
-	public void convertAllFilesToPNG(){
+	public void convertAllFilesToPNG() {
 	  URL url = this.getClass().getClassLoader().getResource("sprites/");
 	  File folder = new File(url.getFile());
 	  File[] listOfFiles = folder.listFiles();
-	  for(File f : listOfFiles){
+	  for (File f : listOfFiles) {
 	    convertBitmapToPNG(f.getName());
 	  }
 	}
 	
 	//This outputs to bin... might want to just hard code the output directory...
-	public void convertBitmapToPNG(String fileName){
-	  try{
+	public void convertBitmapToPNG(String fileName) {
+	  try {
 	    URL url = this.getClass().getClassLoader().getResource("sprites/" + fileName);
 	    String[] newFileName = fileName.split("\\.");
 	    BufferedImage imageBuffer = ImageIO.read(url);
 	    URL newUrl = this.getClass().getClassLoader().getResource("sprites/");
 	    String newFilePath = newUrl.getFile();
 	    File outputfile = new File(newFilePath + newFileName[0] + ".png");
-	    if(!outputfile.exists()){
+	    if (!outputfile.exists()) {
 	      outputfile.mkdirs();
 	      ImageIO.write(imageBuffer, "png", outputfile);
 	    }
     } catch (IOException e) {
-      e.printStackTrace();  // TODO UH OH...error handling...
+      e.printStackTrace();
     }
 	}
 	
