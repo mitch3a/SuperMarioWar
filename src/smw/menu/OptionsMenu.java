489c777c38b3d4628ccc7c87b1f2115fed038cfa
mitch3a <mitchkendall@gmail.com>
2014-03-14 16:44:18 -0400

    Test commit on little computer

---
+++ src/smw/menu/OptionsMenu.java
@@ -0,0 +1,21 @@
+package smw.menu;
+
+import java.awt.Graphics2D;
+import java.awt.image.ImageObserver;
+
+public class OptionsMenu extends Menu {
+
+  @Override
+  public void draw(Graphics2D g, ImageObserver io) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void update(boolean up, boolean down, boolean left, boolean right,
+      boolean select, boolean esc) {
+    // TODO Auto-generated method stub
+    
+  }
+
+}
